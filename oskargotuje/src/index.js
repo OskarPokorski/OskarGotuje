46628b5b40f2081b1d360c733b6511fb73620b1c
OskarPokorski <66873704+OskarPokorski@users.noreply.github.com>
2020-08-20 15:08:16 +0200

    Files preparation

--- oskargotuje/src/index.js
+++ oskargotuje/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './scss/main.scss'
+
+import Header from "./header";
+import Slider from "./slider";
+import Recipies from "./recipies";
+import Form from "./form";
+import Footer from "./footer";
+
 const App = () => {
 
     return (
-        <h1>Oskar Gotuje</h1>
+        <>
+            <Header/>,
+            <Slider/>,
+            <Recipies/>,
+            <Form/>,
+            <Footer/>
+        </>
     )
 }
 
---
+++ oskargotuje/src/recipies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+const Recipies = () => {
+    return null;
+}
+export default Recipies
