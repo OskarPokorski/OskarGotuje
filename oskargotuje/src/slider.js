12a396fdd1b5d719e3aacd7f0ae14ff5f293626d
OskarPokorski <66873704+OskarPokorski@users.noreply.github.com>
2020-08-28 13:42:57 +0200

    Done

--- oskargotuje/src/slider.js
+++ oskargotuje/src/slider.js
@@ -5,13 +5,18 @@ const Slider = () => {
     return (
         <div className="slider">
             <div className="container">
-                <div className={"slider-img"}>
-                </div>
-                <div className={"slider-text"}>
-                    <p className={"slider-text-category"}>śniadanie</p>
-                    <h1 className={"slider-text-title"}>Wegańskie pancakes doskonałe</h1>
-                    <p className={"slider-text-text"}>Pancakes, czyli klasyczne amerykańskie naleśniki, wydawały mi się przereklamowane – małe, blade, gołe placki z importu.</p>
-                    <a  href={"#"} className={"slider-text-more"}>Czytaj dalej >> </a>
+                <div className={"slider-box"}>
+                    <div className={"slider-img"}>
+                    </div>
+                    <div className={"slider-text"}>
+                        <p className={"slider-text-more"}>WKRÓTCE </p>
+                        <h1 className={"slider-text-title"}>Wegańskie pancakes doskonałe</h1>
+                        <p className={"slider-text-text"}>Pancakes są inne niż wszystkie tradycyjne polskie naleśniczki
+                            oraz placuszki. Mniejsze i grubsze niż naleśniki, delikatniejsze niż placki, podobne
+                            zupełnie do niczego. Skórka chociaż przyjemna jest niepozorna, lekko złota, przypieczona,
+                            ale nie chrupiąca.</p>
+
+                    </div>
                 </div>
             </div>
         </div>
